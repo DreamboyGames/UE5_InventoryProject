d51ad7409a4a5f308767a25adf82cd447a8a2f96
Trent <waffledragon97@gmail.com>
2026-09-13 12:37:01 +1000

    First Commit. Item Trace and highlight

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,122 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Inv_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Inventory.h"
+#include "Interaction/Inv_Highlightable.h"
+#include "Items/Components/Inv_ItemComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Widgets/HUD/Inv_HUDWidget.h"
+
+AInv_PlayerController::AInv_PlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	ItemTraceChannel = ECollisionChannel::ECC_GameTraceChannel1;
+	TraceLength = 500.0;
+}
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CreateHUDWidget();
+}
+
+void AInv_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* MappingContext : DefaultIMCs)
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+		}
+	}
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &ThisClass::PrimaryInteract);
+}
+
+void AInv_PlayerController::CreateHUDWidget()
+{
+	if (!IsLocalController()) return;
+	HUDWidget = CreateWidget<UInv_HUDWidget>(this, HUDWidgetClass);
+	if (IsValid(HUDWidget))
+	{
+		HUDWidget->AddToViewport();
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// TICK
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+void AInv_PlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	TraceForItem();
+}
+
+void AInv_PlayerController::TraceForItem()
+{
+	if (!IsValid(GEngine) || !IsValid(GEngine->GameViewport)) return;
+	
+	FVector2D ViewportSize;
+	GEngine->GameViewport->GetViewportSize(ViewportSize);
+	const FVector2D ViewportCenter = ViewportSize / 2.f;
+	
+	FVector TraceStart;
+	FVector Forward;
+	if (!UGameplayStatics::DeprojectScreenToWorld(this, ViewportCenter, TraceStart, Forward)) return;
+	
+	const FVector TraceEnd = TraceStart + Forward * TraceLength;
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ItemTraceChannel);
+	
+	LastHitActor = HitActor;
+	HitActor = HitResult.GetActor();
+	
+	if (!HitActor.IsValid())
+	{
+		if (IsValid(HUDWidget)) HUDWidget->HidePickupMessage();
+	}
+	
+	// If trace still hitting the same actor from last frame, return.
+	if (HitActor == LastHitActor) return;
+	
+	if (HitActor.IsValid())
+	{
+		UInv_ItemComponent* ItemComponent = HitActor->FindComponentByClass<UInv_ItemComponent>();
+		if (!IsValid(ItemComponent)) return;
+		
+		if (IsValid(HUDWidget)) HUDWidget->ShowPickupMessage(ItemComponent->GetPickupMessage());
+		if (UActorComponent* Highlightable = HitActor->FindComponentByInterface(UInv_Highlightable::StaticClass()); IsValid(Highlightable))
+		{
+			IInv_Highlightable::Execute_Highlight(Highlightable);
+		}
+	}
+	
+	if (LastHitActor.IsValid())
+	{
+		if (IsValid(HUDWidget)) HUDWidget->HidePickupMessage();
+		if (UActorComponent* Highlightable = LastHitActor->FindComponentByInterface(UInv_Highlightable::StaticClass()); IsValid(Highlightable))
+		{
+			IInv_Highlightable::Execute_UnHighlight(Highlightable);
+		}
+	}
+}
+
+//END_TICK/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void AInv_PlayerController::PrimaryInteract()
+{
+	UE_LOG(LogTemp, Warning, TEXT("PrimaryInteract"));
+}
+
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Inv_PlayerController.generated.h"
+
+class UInv_HUDWidget;
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AInv_PlayerController : public APlayerController {
+	GENERATED_BODY()
+	
+public:
+	AInv_PlayerController();
+	
+	virtual void Tick(float DeltaSeconds) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+	
+private:
+	
+	void PrimaryInteract();
+	void CreateHUDWidget();
+	
+	void TraceForItem();
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TSubclassOf<UInv_HUDWidget> HUDWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UInv_HUDWidget> HUDWidget;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	double TraceLength;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel;
+	
+	
+	// UPROPERTY prevents a variable from being garbage collected.
+	// TWeakObjectPtr does not affect GC status
+	TWeakObjectPtr<AActor> HitActor;
+	TWeakObjectPtr<AActor> LastHitActor;
+};
